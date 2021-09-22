aba0876bb7f7dd091e56681020d9b6c94399eea3
Damian P <panosovski@gmail.com>
2021-09-22 18:16:09 +0300

    The mechanism of the method has been redesigned.

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -11,31 +11,17 @@ public class CheckPalindrome {
 
         String userWord = UserInput.readUserString();
 
-        String answer = isPalindrome(userWord) ? "Да." : "Нет.";
-        System.out.println("- Слово \"" + userWord + "\" палиндром?\n" + "- " + answer);
+        String answer = isPalindrome(userWord) ? "Yes." : "No.";
+        System.out.println("- \"" + userWord + "\" is palindrome?\n" + "- " + answer);
     }
 
     public static boolean isPalindrome(String word) {
 
-        // Разбинение слова на две части с учётом количества символов.
-        String firstPart = "";
-        String secondPart = word.substring(word.length() / 2);
-
-        if (word.length() % 2 == 0) {
-            firstPart = word.substring(0, word.length() / 2);
-        } else {
-            firstPart = word.substring(0, word.length() / 2 + 1);
-        }
-
-        // Сравниваем строки игнорируя регистр.
-        return firstPart.equalsIgnoreCase(reversePart(secondPart));
-    }
-
-    public static String reversePart(String secondPart) {
-
-        // Переворот второй части, приведение объекта к строке.
-        StringBuilder builder = new StringBuilder(secondPart);
-        return builder.reverse().toString();
+        // Создание объекта класса StringBuilder, использование
+        // методов reverse() и toString().
+        StringBuilder builder = new StringBuilder(word);
+        builder.reverse();
+        return word.equalsIgnoreCase(builder.toString());
     }
 }
 
