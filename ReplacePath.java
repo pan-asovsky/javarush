7b0f68cef3ae7539f45067d47f8b38fa1d96c1b3
Damian P <panosovski@gmail.com>
2021-09-22 20:11:54 +0300

    A ReplacePath has been created that implements the changePath() method

--- ReplacePath.java
+++ ReplacePath.java
@@ -7,7 +7,7 @@ package javarush;
 public class ReplacePath {
 
     public static void main(String[] args) {
-        String path = "/usr/java/jdk1.8/bin";
+        String path = "/usr/java/jdk1.9/bin";
 
         String jdk13 = "jdk-13";
         System.out.println(changePath(path, jdk13));
