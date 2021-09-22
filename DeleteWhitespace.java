f55a78576b897077a3e822dcbf8eb67092b46241
Damian P <panosovski@gmail.com>
2021-09-22 12:20:46 +0300

    DeleteWhitespace added to repository

---
+++ DeleteWhitespace.java
@@ -0,0 +1,32 @@
+package javarush;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Удаление пробелов.
+ */
+
+public class DeleteWhitespace  {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(deleteWhitespace(userInput()));
+    }
+
+    public static String userInput() throws IOException {
+
+        // Создание объекта класса BufferedReader с аргументом
+        // в виде объекта класса InputStreamReader, в который
+        // передаётся текущий поток ввода (System.in).
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+    }
+
+    public static String deleteWhitespace(String userText) {
+
+        // Удаление пробелов методом replace() класса String.
+        return userText.replace(" ", "");
+    }
+}
