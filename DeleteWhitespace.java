9c3dfa77ebc50afff1ca4cecbb551995ccb5877e
Damian P <panosovski@gmail.com>
2021-09-24 11:59:33 +0300

    All other logic has been removed. Only the method of the same name is left.

--- DeleteWhitespace.java
+++ DeleteWhitespace.java
@@ -1,30 +1,10 @@
 package javarush;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /*
 Удаление пробелов.
  */
 
 public class DeleteWhitespace  {
-
-    public static void main(String[] args) throws IOException {
-
-        System.out.println(deleteWhitespace(userInput()));
-    }
-
-    public static String userInput() throws IOException {
-
-        // Создание объекта класса BufferedReader с аргументом
-        // в виде объекта класса InputStreamReader, в который
-        // передаётся текущий поток ввода (System.in).
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String userText = reader.readLine();
-        reader.close();
-        return userText;
-    }
 
     public static String deleteWhitespace(String userText) {
 
