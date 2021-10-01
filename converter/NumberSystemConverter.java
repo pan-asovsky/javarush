4a7f8c3bb40a1741480449550dd2de02b0ba0eb9
Damian P <panosovski@gmail.com>
2021-10-01 14:34:44 +0300

    Изменения внешнего вида кода. Добавлены переменные содержащие строки вывода. Строки в цикле заменены на переменные.

--- converter/NumberSystemConverter.java
+++ converter/NumberSystemConverter.java
@@ -11,259 +11,225 @@ import java.util.Scanner;
 
 public class NumberSystemConverter {
 
+	private static final String shutdown = "Завершение работы.";
+	private static final String continueWork = """
+						Продолжить?
+						1 - Да.
+						0 - Выход.
+						""";
+	private static final String binaryNum = "Введите двоичное число: ";
+	private static final String octalNum = "Введите восьмеричное число: ";
+	private static final String decimalNum = "Введите десятичное число: ";
+	private static final String hexNum = "Введите шестнадцатеричное число: ";
+
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		Scanner sc = new Scanner(System.in);
 
 		while (true) {
-			System.out.println("Доступные конвертации:\n" +
-					"1.  Восьмеричное в десятичное.\n" +
-					"2.  Десятичное в восьмеричное.\n" +
-					"3.  Двоичное в десятичное\n" +
-					"4.  Десятичное в двочное.\n" +
-					"5.  Шестнадцатеричное в десятичное.\n" +
-					"6.  Десятичное в шестнадцатеричное.\n" +
-					"7.  Двоичное в восьмеричнре.\n" +
-					"8.  Восьмеричное в двоичное.\n" +
-					"9.  Восьмеричное в шестнадцатеричное.\n" +
-					"10. Шестнадцатеричное в восьмеричное.\n" +
-					"11. Двоичное в шестнадцатеричное.\n" +
-					"12. Шестнадцатеричное в двоичное.\n" +
-					"0.  Выход.\n\n" +
-					"Введите число соответствующее вашему выбору: ");
+			System.out.println("""
+					Доступные конвертации:
+					1.  Восьмеричное в десятичное.
+					2.  Десятичное в восьмеричное.
+					3.  Двоичное в десятичное
+					4.  Десятичное в двочное.
+					5.  Шестнадцатеричное в десятичное.
+					6.  Десятичное в шестнадцатеричное.
+					7.  Двоичное в восьмеричнре.
+					8.  Восьмеричное в двоичное.
+					9.  Восьмеричное в шестнадцатеричное.
+					10. Шестнадцатеричное в восьмеричное.
+					11. Двоичное в шестнадцатеричное.
+					12. Шестнадцатеричное в двоичное.
+					0.  Выход.
+
+					Введите число соответствующее вашему выбору:\s""");
 
 			int userChoice = sc.nextInt();
 
 			if (userChoice == 0) {
 				sc.close();
-				System.out.println("Завершение работы.");
+				System.out.println(shutdown);
 				break;
 
 			} else if (userChoice == 1) {
 
-				System.out.println("Введите восьмеричное число: ");
+				System.out.println(octalNum);
 				int octalNumber = sc.nextInt();
 				System.out.println("8(" + octalNumber
 						+ ") = 10(" + DecimalOctal.toDecimal(octalNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-									"1 - Да.\n" +
-									"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 2) {
 
-				System.out.println("Введите десятичное число: ");
+				System.out.println(decimalNum);
 				int decimalNumber = sc.nextInt();
 				System.out.println("10(" + decimalNumber
 						+ ") = 8(" + DecimalOctal.toOctal(decimalNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
+
 
 			} else if (userChoice == 3) {
 
-				System.out.println("Введите двоичное число: ");
-				String binaryNumber = bufferedReader.readLine();
+				System.out.println(binaryNum);
+				String binaryNumber = reader.readLine();
 				System.out.println("2(" + binaryNumber
 						+ ") = 10(" + DecimalBinary.toDecimal(binaryNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 4) {
 
-				System.out.println("Введите десятичное число: ");
+				System.out.println(decimalNum);
 				int decimalNumber = sc.nextInt();
 				System.out.println("10(" + decimalNumber
 						+ ") = 2(" + DecimalBinary.toBinary(decimalNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 5) {
 
-				System.out.println("Введите шестнадцатеричное число: ");
-				String hexNumber = bufferedReader.readLine();
+				System.out.println(hexNum);
+				String hexNumber = reader.readLine();
 				System.out.println("16(" + hexNumber
 						+ ") = 10(" + DecimalHex.toDecimal(hexNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 6) {
 
-				System.out.println("Введите десятичное число: ");
+				System.out.println(decimalNum);
 				int decimalNumber = sc.nextInt();
 				System.out.println("10(" + decimalNumber
 						+ ") = 16(" + DecimalHex.toHex(decimalNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 7) {
 
-				System.out.println("Введите двоичное число: ");
-				String binaryNumber = bufferedReader.readLine();
+				System.out.println(binaryNum);
+				String binaryNumber = reader.readLine();
 				System.out.println("2(" + binaryNumber +  ") = 8("
 						+ DecimalOctal.toOctal(DecimalBinary.toDecimal(binaryNumber)) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 8) {
 
-				System.out.println("Введите восьмеричное число: ");
+				System.out.println(octalNum);
 				int octalNumber = sc.nextInt();
 				System.out.println("8(" + octalNumber + ") = 2("
 						+ DecimalBinary.toBinary(DecimalOctal.toDecimal(octalNumber)) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 9) {
 
-				System.out.println("Введите восьмеричное число: ");
+				System.out.println(octalNum);
 				int octalNumber = sc.nextInt();
 				System.out.println("8(" + octalNumber + ") = 16("
 						+ DecimalHex.toHex(DecimalOctal.toDecimal(octalNumber)) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 10) {
 
-				System.out.println("Введите шестнадцатеричное число: ");
-				String hexNumber = bufferedReader.readLine();
+				System.out.println(hexNum);
+				String hexNumber = reader.readLine();
 				System.out.println("16(" + hexNumber + ") = 8("
 						+ DecimalOctal.toOctal(DecimalHex.toDecimal(hexNumber)) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 11) {
 
-				System.out.println("Введите двоичное число: ");
-				String binaryNumber = bufferedReader.readLine();
+				System.out.println(binaryNum);
+				String binaryNumber = reader.readLine();
 				System.out.println("2(" + binaryNumber + ") = 16(" + BinaryHex.toHex(binaryNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 
 			} else if (userChoice == 12) {
 
-				System.out.println("Введите шестнадцатеричное число: ");
-				String hexNumber = bufferedReader.readLine();
+				System.out.println(hexNum);
+				String hexNumber = reader.readLine();
 				System.out.println("16(" + hexNumber + ") = 2(" + BinaryHex.toBinary(hexNumber) + ").");
+				System.out.println(continueWork);
 
-				System.out.println("Продолжить?\n" +
-						"1 - Да.\n" +
-						"0 - Выход.\n");
 				int userChoiceToContinue = sc.nextInt();
 				if (userChoiceToContinue == 0) {
-					System.out.println("Завершение работы.");
+					System.out.println(shutdown);
 					sc.close();
 					break;
-				} else if (userChoiceToContinue == 1) {
-					continue;
 				}
 			}
 		}
